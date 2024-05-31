2f0b258f7f85788d04d9e1f1b8f8bca22fc5b9ef
daniramadhann11 <daniramadhann11@gmail.com>
2024-05-31 14:29:51 +0700

    update: halaman mahasiswa

---
+++ src/components/Tables/TableMahasiswa.jsx
@@ -0,0 +1,76 @@
+import { IconButton } from "@material-tailwind/react";
+import { FiEdit } from "react-icons/fi";
+import { MdDelete } from "react-icons/md";
+
+// eslint-disable-next-line react/prop-types
+const TableMahasiswa = ({ onDeleteItem, onEditItem }) => {
+  const handleDelete = () => {
+    onDeleteItem();
+  };
+
+  const handleEdit = () => {
+    onEditItem();
+  };
+  const TABLE_HEAD = ["No", "NIM", "Nama", "Program Studi", "Foto", "Aksi"];
+  return (
+    <div className="bg-white rounded-lg h-96 lg:h-fit overflow-auto my-5">
+      <table className="min-w-full text-sm text-left text-blue">
+        <thead>
+          <tr>
+            {TABLE_HEAD.map((head, index) => (
+              <th key={index} scope="col" className={`px-4 py-3`}>
+                {head}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          <tr className="odd:bg-blue-gray-50">
+            <td scope="row" className={`px-4 py-3`}>
+              1
+            </td>
+            <td scope="row" className={`px-4 py-3`}>
+              2113221018
+            </td>
+            <td scope="row" className={`px-4 py-3`}>
+              Dani Ramadhan
+            </td>
+            <td scope="row" className={`px-4 py-3`}>Teknik Informatika</td>
+            <td scope="row" className={`px-4 py-3`}></td>
+            <td scope="row" className="flex gap-2 px-4 py-3">
+              <IconButton size="sm" className="bg-navy" onClick={handleEdit}>
+                <FiEdit />
+              </IconButton>
+              <IconButton size="sm" className="bg-red" onClick={handleDelete}>
+                <MdDelete />
+              </IconButton>
+            </td>
+          </tr>
+          <tr className="odd:bg-blue-gray-100">
+            <td scope="row" className={`px-4 py-3`}>
+              2
+            </td>
+            <td scope="row" className={`px-4 py-3`}>
+              2113221018
+            </td>
+            <td scope="row" className={`px-4 py-3`}>
+              Dani Ramadhan
+            </td>
+            <td scope="row" className={`px-4 py-3`}>Teknik Informatika</td>
+            <td scope="row" className={`px-4 py-3`}></td>
+            <td scope="row" className="flex gap-2 px-4 py-3">
+              <IconButton size="sm" className="bg-navy" onClick={handleEdit}>
+                <FiEdit />
+              </IconButton>
+              <IconButton size="sm" className="bg-red" onClick={handleDelete}>
+                <MdDelete />
+              </IconButton>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default TableMahasiswa;
